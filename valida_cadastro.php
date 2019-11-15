2bfe4efcd9aa3fb689e62c4850971e4492f1a02c
Guilherme Schubert Medeiros <guilherme.medeiros@globalmind.local>
2019-11-15 17:36:03 -0300

    Efetuado o insert no valida cadastro

--- valida_cadastro.php
+++ valida_cadastro.php
@@ -1,18 +1,10 @@
 <?php
+include("includes/conexao.php");
 $email = $_POST['email'];
 $confirmaemail = $_POST['confirmaemail'];
 $usuario = $_POST['username'];
 $senha = $_POST['senha'];
 $sexo = $_POST['sexo'];
-
-$sql = "CREATE TABLE Usuarios (
-id INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-email VARCHAR(30) NOT NULL,
-username VARCHAR(30) NOT NULL,
-senha VARCHAR(50),
-sexo INT(2),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
 
 if($email != $confirmaemail){
 	echo "ERRO (Emails não coincidem !!)</br>";
@@ -53,6 +45,13 @@ function criptografa_senha($senha) {
    return $senhacrip;
 }
 
+$sql = "INSERT INTO usuario VALUES (1,2,'$usuarionobanco', '$emailnobanco', '$senhanobanco','$sexo')"; 
+		
+	$query = mysqli_query($serv,$sql); 
+		if ($query) 
+				echo "Sucesso ao inserir usuario no banco"; 
+        else 
+                echo "Erro ao inserir registro"; 
 
 function contem_numero($usuario) {
    return is_numeric(filter_var($usuario, FILTER_SANITIZE_NUMBER_INT));
